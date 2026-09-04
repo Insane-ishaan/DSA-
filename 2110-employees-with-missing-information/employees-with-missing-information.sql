dbb58df5011c379b2b30c039350fe7e1a28a9a79
Ishaan Sharma <ishaanshama93@gmail.com>
2026-09-04 18:01:37 +0530

    Time: 558 ms (86.06%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,6 +1,6 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 SELECT
-*
+t.employee_id
 FROM
 (SELECT 
 (CASE WHEN e.name IS NULL AND s.salary IS NOT NULL THEN s.employee_id END) AS employee_id
@@ -12,6 +12,6 @@ UNION
 SELECT
 (CASE WHEN  s.salary IS NULL AND e.name IS NOT NULL THEN e.employee_id END) AS employee_id
 FROM Employees e LEFT JOIN Salaries s
-ON e.employee_id = s.employee_id)
-WHERE employee_id IS NOT NULL
-ORDER BY employee_id;
+ON e.employee_id = s.employee_id) AS t
+WHERE t.employee_id IS NOT NULL
+ORDER BY t.employee_id;
