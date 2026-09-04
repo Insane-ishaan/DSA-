2d43455e85578ae857236ff899724f9f0800210d
Ishaan Sharma <ishaanshama93@gmail.com>
2026-09-04 15:30:36 +0530

    Time: 1 ms (57.54%) | Memory: 47.6 MB (67.39%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,28 +1,23 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int leftMax = height[0];
-        int LGE[] = new int[n];
-        LGE[0] = leftMax;
-        int rightMax = height[n - 1];
-        int RGE[] = new int[n];
-        RGE[n - 1] = rightMax;
-
-        for (int i = 1; i < n; i++) {
-            leftMax = Math.max(leftMax, height[i]);
-            LGE[i] = leftMax;
-        }
-
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax = Math.max(rightMax, height[i]);
-            RGE[i] = rightMax;
-        }
-
+        int left = 0;
+        int right = n - 1;
         int totalWater = 0;
-        for (int i = 0; i < n; i++) {
-            int Boundary = Math.min(LGE[i], RGE[i]);
-            int waterHolds = Boundary - height[i];
-            totalWater += waterHolds;
+
+        int leftMax = 0, rightMax = 0, waterStored = 0;
+
+        while (left <= right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+
+            if (leftMax > rightMax) {
+                waterStored = rightMax - height[right--];
+                totalWater += waterStored;
+            } else {
+                waterStored = leftMax - height[left++];
+                totalWater += waterStored;
+            }
         }
 
         return totalWater;
