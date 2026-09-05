0008d6343c6598fb79701d98dbbde87e293860f1
Ishaan Sharma <ishaanshama93@gmail.com>
2026-09-05 12:27:02 +0530

    Time: 31 ms (26.21%) | Memory: 48.3 MB (88.48%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,74 @@
+class Solution {
+    private int[] getNSL(int h[], int len) {
+        Stack<Integer> st = new Stack<>();
+        int[] NSL = new int[len];
+        for (int i = 0; i < len; i++) {
+            while (!st.isEmpty() && h[st.peek()] >= h[i]) {
+                st.pop();
+            }
+
+            NSL[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        return NSL;
+    }
+
+    private int[] getNSR(int[] h, int len) {
+        Stack<Integer> st = new Stack<>();
+        int[] NSR = new int[len];
+        for (int i = len - 1; i >= 0; i--) {
+            while (!st.isEmpty() && h[st.peek()] >= h[i]) {
+                st.pop();
+            }
+
+            NSR[i] = st.isEmpty() ? len : st.peek();
+            st.push(i);
+        }
+
+        return NSR;
+    }
+
+    private int getMaxArea(int[] h, int len) {
+        int[] NSR = getNSR(h, len);
+        int[] NSL = getNSL(h, len);
+
+        int maxArea = 0;
+        for (int i = 0; i < len; i++) {
+            int width = (NSR[i] - NSL[i]) - 1;
+            int area = h[i] * width;
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int height[] = new int[m];
+        for (int col = 0; col < m; col++) {
+            if (matrix[0][col] == '0') {
+                height[col] = 0;
+            } else {
+                height[col] = 1;
+            }
+        }
+
+        int maxArea = getMaxArea(height, m);
+
+        for (int row = 1; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (matrix[row][col] == '0') {
+                    height[col] = 0;
+                } else {
+                    height[col] += 1;
+                }
+            }
+
+            maxArea = Math.max(maxArea, getMaxArea(height, m));
+        }
+
+        return maxArea;
+    }
+}
